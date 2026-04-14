d4ea724d8fb7e456c36a464c44c199c59104aa20
ramenmac <kevinnguyen4200@gmail.com>
2026-04-14 10:57:30 -0500

    Main function for cellphone, prompt user for information and setters for class

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,4 +1,29 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class CellPhoneApplication {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        CellPhone kevCellphone = new CellPhone();
+
+        System.out.printf("What is the serial number?: ");
+        kevCellphone.setSerialNumber(input.nextInt());
+        input.nextLine();
+
+        System.out.printf("What model is the phone?: ");
+        kevCellphone.setModelName(input.nextLine());
+
+        System.out.printf("Who is the carrier?: ");
+        kevCellphone.setCellCarrier(input.nextLine());
+
+        System.out.printf("What is the phone number?: ");
+        kevCellphone.setPhoneNumber(input.nextLine());
+
+        System.out.printf("Who is the owner of the phone?: ");
+        kevCellphone.setOwnerName(input.nextLine());
+
+
+    }
 }
